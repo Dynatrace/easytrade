36789ecb8037fee61bc090c833d94816fd106c30
Krzysztof Sajko <krzysztof.sajko@dynatrace.com>
2025-03-24 21:55:45 +0100

    chore: add eslint

---
+++ src/frontend/eslint.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+    },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        settings: {
+            react: {
+                version: "detect", // Automatically detect the React version
+            },
+        },
+    },
+])
