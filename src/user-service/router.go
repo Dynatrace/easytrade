76879c167516b99f7453833822900324076486c4
jkunikowski-dt <jakub.kunikowski@dynatrace.com>
2026-07-15 15:12:40 +0200

    feat: refactor logging in router and streamline API route definitions

--- src/user-service/router.go
+++ src/user-service/router.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"dynatrace.com/easytrade/user-service/account"
 	"dynatrace.com/easytrade/user-service/login"
+	"dynatrace.com/easytrade/user-service/utils"
 	"dynatrace.com/easytrade/user-service/version"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
 )
 
 func Logger() gin.HandlerFunc {
+	log := utils.GetSugar()
 	return func(ctx *gin.Context) {
 		startTime := time.Now()
 
 		ctx.Next()
 
-		latency := time.Since(startTime)
-		requestString := fmt.Sprintf("%s %s", ctx.Request.Method, ctx.Request.URL)
-
-		log.WithFields(log.Fields{
-			"request": requestString,
-			"status":  ctx.Writer.Status(),
-			"latency": latency,
-			"ip":      ctx.ClientIP(),
-		}).Info("Request finished")
+		log.Infow("request finished",
+			"method", ctx.Request.Method,
+			"url", ctx.Request.URL.String(),
+			"status", ctx.Writer.Status(),
+			"latency", time.Since(startTime),
+			"ip", ctx.ClientIP(),
+		)
 	}
 }
 
 // CreateRouter builds the gin engine and registers the routes ported from loginservice
-// (excluding logout) and accountservice.
 func CreateRouter() *gin.Engine {
 	router := gin.New()
 
@@ -41,27 +38,16 @@ func CreateRouter() *gin.Engine {
 
 	api := router.Group("/api")
 	{
-		api.POST("/Login", login.Login)
-		api.POST("/Signup", login.Signup)
-
-		accountsGroup := api.Group("/Accounts")
+		auth := api.Group("/auth")
 		{
-			accountsGroup.GET("/GetAccountById/:id", login.GetAccountById)
-			accountsGroup.GET("/GetAccountByUsername/:username", login.GetAccountByUsername)
-			accountsGroup.POST("/CreateNewAccount", login.CreateNewAccount)
+			auth.POST("/login", login.Login)
+			auth.POST("/signup", login.Signup)
 		}
 
-		accountV1 := api.Group("/account")
+		accounts := api.Group("/accounts")
 		{
-			accountV1.GET("/:id", account.GetAccount)
-			accountV1.PUT("/update", account.UpdateAccount)
-		}
-
-		accountV2 := api.Group("/accounts")
-		{
-			accountV2.GET("/presets", account.GetPresets)
-			accountV2.GET("/:id", account.GetAccount)
-			accountV2.PUT("/", account.UpdateAccount)
+			accounts.GET("/presets", account.GetPresets)
+			accounts.GET("/:id", account.GetAccount)
 		}
 
 		api.GET("/version", version.GetVersion)
