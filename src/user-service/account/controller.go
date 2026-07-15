b0d8a000cd9cbafab78cb829cf81c4a029254cd3
jkunikowski-dt <jakub.kunikowski@dynatrace.com>
2026-07-15 08:35:14 +0200

    feat: GetAccount method in controller

--- src/user-service/account/controller.go
+++ src/user-service/account/controller.go
@@ -1,17 +1,33 @@
 package account
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
+
+var managerClient = NewManagerClient()
 
 // GetAccount handles GET /api/account/:id (v1) and GET /api/accounts/:id (v2).
 // Ported from accountservice's AccountController.java / AccountControllerV2.java.
-//
-// TODO: not implemented yet - this is a scaffold-only stub.
 func GetAccount(ctx *gin.Context) {
-	ctx.String(http.StatusNotImplemented, "not implemented")
+	accountIdStr := ctx.Param("id")
+
+	id, err := strconv.Atoi(accountIdStr)
+
+	if err != nil {
+		ctx.String(http.StatusBadRequest, "invalid account id: %v", err)
+		return
+	}
+
+	managerAccount, err := managerClient.GetAccountById(id)
+
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, "failed to get account from manager service: %v", err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, managerAccount)
 }
 
 // UpdateAccount handles PUT /api/account/update (v1) and PUT /api/accounts/ (v2).
