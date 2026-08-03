0efe0a7d7127d52c730764d53d94c958e8433a3f
Maksim Todryk <maksim.todryk@dynatrace.com>
2026-08-03 23:39:03 +0200

    deleted manager service

--- src/manager/easyTradeManager/Models/FeatureFlag.cs
+++
@@ -1,9 +0,0 @@
-public class FeatureFlag
-{
-    public string Id { get; set; }
-    public bool Enabled { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public bool IsModifiable { get; set; }
-    public string Tag { get; set; }
-}
